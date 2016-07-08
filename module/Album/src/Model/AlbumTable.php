d080c0482d6e3e4b19596c2c8d060e68305efabc
Geert Eltink <geert.eltink@gmail.com>
2016-07-08 22:08:11 +0200

    Modifying the AlbumTable

--- module/Album/src/Model/AlbumTable.php
+++ module/Album/src/Model/AlbumTable.php
@@ -4,6 +4,11 @@ namespace Album\Model;
 
 use RuntimeException;
 use Zend\Db\TableGateway\TableGatewayInterface;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\Sql\Select;
+use Zend\Db\TableGateway\TableGateway;
+use Zend\Paginator\Adapter\DbSelect;
+use Zend\Paginator\Paginator;
 
 class AlbumTable
 {
@@ -14,9 +19,37 @@ class AlbumTable
         $this->tableGateway = $tableGateway;
     }
 
-    public function fetchAll()
+    public function fetchAll($paginated = false)
     {
+        if ($paginated) {
+            return $this->fetchPaginatedResults();
+        }
+
         return $this->tableGateway->select();
+    }
+
+    private function fetchPaginatedResults()
+    {
+        // Create a new Select object for the table:
+        $select = new Select($this->tableGateway->getTable());
+
+        // Create a new result set based on the Album entity:
+        $resultSetPrototype = new ResultSet();
+        $resultSetPrototype->setArrayObjectPrototype(new Album());
+
+        // Create a new pagination adapter object:
+        $paginatorAdapter = new DbSelect(
+        // our configured select object:
+            $select,
+            // the adapter to run it against:
+            $this->tableGateway->getAdapter(),
+            // the result set to hydrate:
+            $resultSetPrototype
+        );
+
+        $paginator = new Paginator($paginatorAdapter);
+
+        return $paginator;
     }
 
     public function getAlbum($id)
