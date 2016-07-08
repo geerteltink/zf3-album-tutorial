123fdd721e95fb6b56f955ee9460742e5fb99571
Geert Eltink <geert.eltink@gmail.com>
2016-07-08 10:41:01 +0200

    Listing albums

--- module/Album/src/Controller/AlbumController.php
+++ module/Album/src/Controller/AlbumController.php
@@ -19,6 +19,9 @@ class AlbumController extends AbstractActionController
 
     public function indexAction()
     {
+        return new ViewModel([
+            'albums' => $this->table->fetchAll(),
+        ]);
     }
 
     public function addAction()
