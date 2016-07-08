fe6ea44b6aca86e4ba26e96271f61b3a0581348e
Geert Eltink <geert.eltink@gmail.com>
2016-07-08 10:05:58 +0200

    Setup router

--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -5,11 +5,31 @@ namespace Album;
 use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
-    'controllers'  => [
+    'controllers' => [
         'factories' => [
             Controller\AlbumController::class => InvokableFactory::class,
         ],
     ],
+
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type'    => 'segment',
+                'options' => [
+                    'route'       => '/album[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults'    => [
+                        'controller' => Controller\AlbumController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+
     'view_manager' => [
         'template_path_stack' => [
             'album' => __DIR__ . '/../view',
