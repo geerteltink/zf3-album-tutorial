b3817ba1547c5a01029ddea9f0b9b2d2be81dced
Geert Eltink <geert.eltink@gmail.com>
2016-07-08 17:53:44 +0200

    Your first controller test

---
+++ module/Album/test/Controller/AlbumControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace AlbumTest\Controller;
+
+use Album\Controller\AlbumController;
+use Zend\Stdlib\ArrayUtils;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class AlbumControllerTest extends AbstractHttpControllerTestCase
+{
+    protected $traceError = false;
+
+    public function setUp()
+    {
+        // The module configuration should still be applicable for tests.
+        // You can override configuration here with test case specific values,
+        // such as sample view templates, path stacks, module_listener_options,
+        // etc.
+        $configOverrides = [];
+
+        $this->setApplicationConfig(ArrayUtils::merge(
+        // Grabbing the full application configuration:
+            include __DIR__ . '/../../../../config/application.config.php',
+            $configOverrides
+        ));
+        parent::setUp();
+    }
+
+    public function testIndexActionCanBeAccessed()
+    {
+        $this->dispatch('/album');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('Album');
+        $this->assertControllerName(AlbumController::class);
+        $this->assertControllerClass('AlbumController');
+        $this->assertMatchedRouteName('album');
+    }
+}
