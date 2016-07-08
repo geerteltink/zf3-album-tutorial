56659716807fe873b32617bd98476927337ddf06
Geert Eltink <geert.eltink@gmail.com>
2016-07-08 10:15:48 +0200

    Using ServiceManager to configure the table gateway and inject into the AlbumTable

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,8 @@
  */
 
 return [
-    // ...
+    'db' => [
+        'driver' => 'Pdo',
+        'dsn'    => sprintf('sqlite:%s/data/zftutorial.db', realpath(getcwd())),
+    ],
 ];
--- module/Album/Module.php
+++ module/Album/Module.php
@@ -2,6 +2,9 @@
 
 namespace Album;
 
+use Zend\Db\Adapter\AdapterInterface;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 
 class Module implements ConfigProviderInterface
@@ -9,5 +12,25 @@ class Module implements ConfigProviderInterface
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+                Model\AlbumTable::class        => function ($container) {
+                    $tableGateway = $container->get(Model\AlbumTableGateway::class);
+
+                    return new Model\AlbumTable($tableGateway);
+                },
+                Model\AlbumTableGateway::class => function ($container) {
+                    $dbAdapter          = $container->get(AdapterInterface::class);
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Model\Album());
+
+                    return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
+                },
+            ],
+        ];
     }
 }
