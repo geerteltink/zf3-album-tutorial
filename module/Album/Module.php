a1c5a42a4bb6f4f2574b6aae206a4ea1b4f11ce9
Geert Eltink <geert.eltink@gmail.com>
2016-07-08 10:00:05 +0200

    Setting up the Album module

---
+++ module/Album/Module.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Album;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements ConfigProviderInterface
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+}
